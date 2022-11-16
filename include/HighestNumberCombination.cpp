028d84ed08c2dde11270423912c033578b3a24d1
Dmitri Ranfft <ranfft.dmitri@gmail.com>
2022-11-16 02:23:22 +0100

    Implement a function to determine the length of an unsigned integer

--- include/HighestNumberCombination.cpp
+++ include/HighestNumberCombination.cpp
@@ -1,2 +1,25 @@
 #include "HighestNumberCombination.hpp"
 
+// #include <string>
+// #include <chrono>
+// #include <iostream>
+
+using namespace std;
+
+uint8_t HighestNumberCombination::uintLen(uint64_t num){
+  uint8_t len = 0;
+
+  // auto start = chrono::high_resolution_clock::now();
+  // len = to_string(num).length();
+
+  do{
+     ++len;
+     num /= 10;
+  }while(num);
+
+  // auto runtime = chrono::high_resolution_clock::now() - start;
+  // uint64_t runtime_us = chrono::duration_cast<std::chrono::microseconds>(runtime).count();
+  // cout<<runtime_us<<endl;
+
+  return len;
+}
--- include/HighestNumberCombination.hpp
+++ include/HighestNumberCombination.hpp
@@ -1,6 +1,11 @@
 #ifndef HIGHEST_NUMBER_COMBINATION_HPP
 #define HIGHEST_NUMBER_COMBINATION_HPP
 
+#include <cstdint>
 
+class HighestNumberCombination{
+public:
+  static uint8_t uintLen(uint64_t num);
+};
 
 #endif // HIGHEST_NUMBER_COMBINATION_HPP
