d9152adc5275218b7aa2a5b5c4de605d5668344e
Dmitri Ranfft <ranfft.dmitri@gmail.com>
2022-11-16 02:11:35 +0100

    Implement the HighestNumberCombination class

--- include/HighestNumberCombination.cpp
+++ include/HighestNumberCombination.cpp
@@ -1,8 +1,8 @@
 #include "HighestNumberCombination.hpp"
 
-// #include <string>
 // #include <chrono>
 // #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -22,4 +22,44 @@ uint8_t HighestNumberCombination::uintLen(uint64_t num){
   // cout<<runtime_us<<endl;
 
   return len;
+}
+
+bool HighestNumberCombination::cmp(const string &a, const string &b)
+{
+  // return true if a < b.
+  string ab = a+b;
+  string ba = b+a;
+
+  bool result = false;
+
+  if(ab.compare(ba) < 0){
+    result = true;
+  }
+
+  return result;
+}
+
+vector<string> HighestNumberCombination::uintArrayToStrVector(const uint64_t uintArray[], uint64_t len)
+{
+  vector<string> result;
+
+  for(uint64_t i = 0; i < len; ++i){
+    result.push_back(to_string(uintArray[i]));
+  }
+
+  return result;
+}
+
+string HighestNumberCombination::combine(const uint64_t numbers[], uint64_t len)
+{
+  string result = "";
+  vector<string> data = uintArrayToStrVector(numbers, len);
+
+  // TODO: use radix sort or std::sort depending on the size of the array.
+  sort(data.begin(), data.end(), cmp);
+
+  for(int i = 0; i < data.size(); ++i){
+    result = data[i]+result;
+  }
+  return result;
 }
--- include/HighestNumberCombination.hpp
+++ include/HighestNumberCombination.hpp
@@ -2,10 +2,18 @@
 #define HIGHEST_NUMBER_COMBINATION_HPP
 
 #include <cstdint>
+#include <string>
+#include <vector>
 
 class HighestNumberCombination{
+private:
+  static bool cmp(const std::string &a, const std::string &b);
+  static std::vector<std::string> uintArrayToStrVector(const uint64_t uintArray[], uint64_t len);
+
 public:
   static uint8_t uintLen(uint64_t num);
+
+  static std::string combine(const uint64_t numbers[], uint64_t len);
 };
 
 #endif // HIGHEST_NUMBER_COMBINATION_HPP
