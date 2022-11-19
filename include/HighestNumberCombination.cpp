613f7f5a7d062021b8a281b533079f2818a1ac29
Dmitri Ranfft <ranfft.dmitri@gmail.com>
2022-11-19 04:06:25 +0100

    Remove unused code

--- include/HighestNumberCombination.cpp
+++ include/HighestNumberCombination.cpp
@@ -1,6 +1,5 @@
 #include "HighestNumberCombination.hpp"
 
-// #include <chrono>
 #include <iostream>
 #include <algorithm>
 
@@ -11,17 +10,10 @@ using namespace std;
 uint8_t HighestNumberCombination::uintLen(uint64_t num){
   uint8_t len = 0;
 
-  // auto start = chrono::high_resolution_clock::now();
-  // len = to_string(num).length();
-
   do{
      ++len;
      num /= 10;
   }while(num);
-
-  // auto runtime = chrono::high_resolution_clock::now() - start;
-  // uint64_t runtime_us = chrono::duration_cast<std::chrono::microseconds>(runtime).count();
-  // cout<<runtime_us<<endl;
 
   return len;
 }
